e118f873bd99ea860921ba78d2e886abbb7af2f5
Oshani <oshanis@gmail.com>
2014-07-28 06:02:22 -0400

    added support for mongo rest api

--- web.js
+++ web.js
@@ -1,36 +1,73 @@
 // web.js
 var express = require("express");
+var mongoskin = require('mongoskin');
+var bodyParser = require('body-parser');
 var logfmt = require("logfmt");
+
 var app = express();
 
-// Database Settings
-var MongoClient = require('mongodb').MongoClient;
+app.use(bodyParser());
 
 var mongoUri = process.env.MONGOLAB_URI ||
   process.env.MONGOHQ_URL ||
   'mongodb://localhost:27017/exampleDb';
 
-var resource_log_collection;
-var user_log_collection;
-var derivative_log_collection;
+var db = mongoskin.db(mongoUri, {safe:true})
 
-// Connect to the db
-module.exports = MongoClient.connect(mongoUri, function(err, db) {
-    if(err) { return console.dir(err); }
 
-    //Instantiate the users table to store their data
-    resource_log_collection = db.collection('resource_log');
-});
+ app.use(logfmt.requestLogger());
 
 
 
-app.use(logfmt.requestLogger());
 
-app.get('/', function(req, res) {
-	res.send('HTTPA Provenance Tracker Gateway');
-    });
+
+app.param('collectionName', function(req, res, next, collectionName){
+  req.collection = db.collection(collectionName)
+  return next()
+})
+
+app.get('/', function(req, res, next) {
+  res.send('HTTPA Provenance Tracker Gateway<br> please select a collection, e.g., /collections/messages');
+})
+
+app.get('/collections/:collectionName', function(req, res, next) {
+  req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.post('/collections/:collectionName', function(req, res, next) {
+  req.collection.insert(req.body, {}, function(e, results){
+    if (e) return next(e)
+    res.send(results)
+  })
+})
+
+app.get('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.findById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send(result)
+  })
+})
+
+app.put('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+})
+
+app.del('/collections/:collectionName/:id', function(req, res, next) {
+  req.collection.removeById(req.params.id, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+})
 
 var port = Number(process.env.PORT || 5000);
+
 app.listen(port, function() {
-	console.log("Listening on " + port);
-    });
+
+    console.log("Listening on " + port);
+});
