e19efcaeeef89ff53e53cb605c697de5d25a9b9c
Oshani <oshanis@gmail.com>
2014-07-28 19:43:57 -0400

    added audit log retrieval

--- web.js
+++ web.js
@@ -18,47 +18,108 @@ var db = mongoskin.db(mongoUri, {safe:true})
  app.use(logfmt.requestLogger());
 
 
-
-
-
 app.param('collectionName', function(req, res, next, collectionName){
-  req.collection = db.collection(collectionName)
-  return next()
+  req.collection = db.collection(collectionName);
+  return next();
 })
 
 app.get('/', function(req, res, next) {
-  res.send('HTTPA Provenance Tracker Gateway<br> please select a collection, e.g., /collections/messages');
+  res.send('<h3>HTTPA Provenance Tracker Gateway</h3><br> please select a collection, e.g., /collections/messages');
 })
 
-app.get('/collections/:collectionName', function(req, res, next) {
+
+app.get('/:collectionName', function(req, res, next) {
   req.collection.find({} ,{limit:10, sort: [['_id',-1]]}).toArray(function(e, results){
     if (e) return next(e)
     res.send(results)
   })
 })
 
-app.post('/collections/:collectionName', function(req, res, next) {
+app.post('/:collectionName', function(req, res, next) {
+
+  console.log("Received record for: " + req.body._id);
+  //Check if the req.body has any source attributes. If so, find them and add this _id
+  //to the derivative field
+  if (req.body.source.length > 0){
+    for (var i = 0; i<req.body.source.length; i++){
+        var source = req.body.source[i];
+        console.log(source);
+        req.collection.findById(source, function(e, result){
+            if (e) { return next(e); }
+            req.collection.updateById(source, {$push:{"derivatives" : req.body._id}}, {safe:true, multi:false}, function(e, result){
+                if (e) return next(e)
+                console.log((result===1)?{msg:'added this as a derivative'}:{msg:'error'})
+                });
+        });
+    }
+  }
   req.collection.insert(req.body, {}, function(e, results){
-    if (e) return next(e)
+    if (e) {
+        res.send("Error occured. Possibly you attempted adding a duplicate record.");
+        return next(e)
+    }
     res.send(results)
   })
 })
 
-app.get('/collections/:collectionName/:id', function(req, res, next) {
+app.get('/:collectionName/:id', function(req, res, next) {
   req.collection.findById(req.params.id, function(e, result){
     if (e) return next(e)
     res.send(result)
   })
 })
 
-app.put('/collections/:collectionName/:id', function(req, res, next) {
-  req.collection.updateById(req.params.id, {$set:req.body}, {safe:true, multi:false}, function(e, result){
+app.put('/:collectionName/:id', function(req, res, next) {
+    //Having $set here, will replace the record with the new values
+  req.collection.updateById(req.params.id, {$push:req.body}, {safe:true, multi:false}, function(e, result){
+    if (e) return next(e)
+    res.send((result===1)?{msg:'success'}:{msg:'error'})
+  })
+});
+
+//Any generic activity, in the URL pattern specify what activity you support
+app.put('/:collectionName/:activity/:id', function(req, res, next) {
+  
+
+  req.collection.updateById(req.params.id, {$push: {"activity": {"name" : req.params.activity, "time" : new Date(), "details" : req.body}}}, {safe:true, multi:false}, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
   })
 })
 
-app.del('/collections/:collectionName/:id', function(req, res, next) {
+
+
+
+//Audit URIS
+app.get('/:collectionName/audit/:id', function(req, res, next){
+    req.collection.findById(req.params.id, function(e, result){
+        if (e) return next(e)
+
+        if (result.activity != undefined){
+            result.activity.sort(
+                function (a,b) {
+                  if (a != null && b != null){
+                      if (a.time > b.time)
+                         return -1;
+                      if (a.time < b.time)
+                        return 1;
+                      return 0;  
+                  }
+                  
+                });
+
+            res.send(result.activity);        
+        }
+        else {
+            res.send("not found");
+        }
+
+  })
+ 
+
+});
+
+app.del('/:collectionName/:id', function(req, res, next) {
   req.collection.removeById(req.params.id, function(e, result){
     if (e) return next(e)
     res.send((result===1)?{msg:'success'}:{msg:'error'})
